3731e9e09a2c4b06b057b1de6127231ae1723e93
worsleyj <josephworsley23@gmail.com>
2024-08-27 12:39:17 -0400

    add iterative fibonacci

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
 console.log("test");
+
+function fibs(num) {
+  let index = 2;
+  const arr = [0, 1];
+  while (index < num) {
+    arr[index] = arr[index - 1] + arr[index - 2];
+    index++;
+  }
+  return arr;
+}
+
+console.log(fibs(8));
